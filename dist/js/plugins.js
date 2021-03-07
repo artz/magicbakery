cb429ca28980deba2f5d1b5824c4afb812b23ca3
Dave Artz <daveartz@gmail.com>
2021-03-07 14:18:19 -0500

    Order form.

--- dist/js/plugins.js
+++ dist/js/plugins.js
@@ -1,24 +1,35 @@
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-  var method;
-  var noop = function () {};
-  var methods = [
-    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-  ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
+(function($) {
 
-  while (length--) {
-    method = methods[length];
+$.fn.magicQuantity = function() {
+  let total = 0
+  return this.each((i, el) => {
+    let count = 0
+    const $el = $(el)
+    const $div = $('<div class="quantity"></div>')
+    const $add = $('<button>+</button>')
+    const $subtract = $('<button>-</button>')
+    const $quantity = $('<span>0</span>')
 
-    // Only stub undefined methods.
-    if (!console[method]) {
-      console[method] = noop;
-    }
-  }
-}());
+    $el.click(() => {
+      $quantity.text(count)
+      $el.trigger('total', total)
+    })
+    $add.click(() => {
+      count += 1
+      total += 1
+    })
+    $subtract.click(() => {
+      if (count > 0) {
+        count -= 1
+        total -= 1
+      }
+    })
 
-// Place any jQuery/helper plugins in here.
+    $div.append($subtract)
+    $div.append($quantity)
+    $div.append($add)
+    $el.append($div)
+  })
+}
+
+}(jQuery));
